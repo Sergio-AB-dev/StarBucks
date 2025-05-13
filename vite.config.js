26371a08dcf2dcedbb8b63ef03d3ae7ab1de916a
Sergio Ardila <sardila535@gmail.com>
2025-05-13 10:18:17 -0500

    fix 404 gh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/StarBucks/',
+  base: process.env.NODE_ENV === 'production' ? '/StarBucks/' : '/',
   plugins: [react()],
 })
